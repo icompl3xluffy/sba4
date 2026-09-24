404d2166d17620b12e0516d8ad21c8a0aafd58c5
icompl3xluffy <bertyneganthier@yahoo.com>
2026-09-23 21:30:03 -0400

    items are able to add to the screen

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
 //Create an empty array called shoppingList.
-// let toDoList = [];
-let taskName= document.getElementById("task");
-let categoryInfo=document.getElementById("category");
-let statusInfo=document.getElementById("status")
-let date= document.getElementById("dueDate")
-let btn=document.getElementById("add")
-let toDoList=[]
 
-let item={
-    taskName :document.getElementById("task")
-    categoryInfo : document.getElementById("category")
-    statusInfo :document.getElementById("status")
-    date :document.getElementById("dueDate")
-}
+let taskName = document.getElementById("task");
+let categoryInfo = document.getElementById("category");
+let statusInfo = document.getElementById("status");
+let dateInfo = document.getElementById("dueDate");
+let btn = document.getElementById("add");
+let list = document.getElementById("list");
+let toDoList = [];
+const options=`<option value="overdue">OverDue</option>
+                        <option value="Not-Started">Not-Started</option>
+                        <option value="InProgress">InProgress</option>
+                        <option value="Completed">Completed</option>`
+
+btn.addEventListener("click", (e) => {
+  let item = {
+    taskName: taskName.value,
+    categoryInfo: categoryInfo.value,
+    statusInfo: statusInfo.value,
+    dateInfo: dateInfo.value,
+  };
+  if (item.taskName === "") {
+    alert("Please enter an item.");
+    return;
+  } else {
+    toDoList.push(item);
+    console.log(toDoList);
+  }
+  render()
+  // console.log(item)
+});
+
+function render() {
+    list.innerHTML=""
+  for (let i = 0; i < toDoList.length; i++) {
+    console.log(toDoList[i])
+    let itemList = document.createElement("li");
+    let status = document.createElement("select");
+        
+
+    let title = document.createElement("p");
+    let date = document.createElement("p");
+    let category = document.createElement("p");
     
+    title.innerText= toDoList[i].taskName
+    status.innerHTML= options
+    status.value= toDoList[i].statusInfo
+    date.innerText= toDoList[i].dateInfo
+    category.innerText= toDoList[i].categoryInfo
 
-  
-// //Write a function called addItem that takes an item as a parameter and adds it to the shoppingList array.
-// function addItem() {
-    
-function addItem(item) {
-    toDoList.push(item); // Add item to shopping list
-    console.log(toDoListt)
 
+    itemList.appendChild(status);
+    itemList.appendChild(title);
+    itemList.appendChild(date);
+    itemList.appendChild(category);
+    list.appendChild(itemList);
+  }
 }
 
-function updateList(additem()){
-    toDoList.update
-}
-
-
-btn.addEventListener("click", function addItem()) {
-    // let item = itemInput.value;
-    if (item === "") {
-    // alert("Please enter an item.");
-    return
-    }
-}
-toDoList.push(item); // Add item to list
-console.log(toDoList);
-
-function filterItems(item) {
-
-19
-
-// function displayList() {
-//     for (let item of toDoList) {
-//         console.log(toDoList)
-// }
-// }
+let filter=Document.getElementById(filter)
+ 
